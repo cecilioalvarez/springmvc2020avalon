3375d02d16d12642570390326868456b037c62af
Formacion <Formacion@DESKTOP-E5B1ANP.technicolor.net>
2020-04-20 12:36:15 +0000

    commit

--- mvc2/src/main/java/es/avalon/mvc/controladores/LibrosController.java
+++ mvc2/src/main/java/es/avalon/mvc/controladores/LibrosController.java
@@ -1,12 +1,23 @@
 package es.avalon.mvc.controladores;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.avalon.dominio.Categoria;
+import es.avalon.dominio.Libro;
+import es.avalon.repositorios.LibroRepository;
+import es.avalon.servicios.ServicioLibros;
 
 @Controller
 @RequestMapping("/libros")
 public class LibrosController {
 	
+	@Autowired
+	private ServicioLibros servicio;
 	// /libros/hola
 	@RequestMapping("/hola")
 	public String hola() {
@@ -15,4 +26,45 @@ public class LibrosController {
 		return "hola";
 	}
 
+	@RequestMapping("/libro")
+	public String libro(Model modelo) {
+		modelo.addAttribute("libro",new Libro("1","java","pedro",20));
+		return "paginaLibro";
+	}
+//	@RequestMapping("/listaLibros")
+//	public String listaLibros (Model modelo) {
+//		ArrayList<Libro> lista=new ArrayList<Libro>();
+//		lista.add(new Libro("1","java","pedro",20));
+//		lista.add(new Libro("2","net","ana",30));
+//		modelo.addAttribute("lista",lista);
+//		return "listaLibros";
+//	}
+	
+	@RequestMapping("/listaLibros")
+	public String listaLibros (Model modelo) {
+		
+		modelo.addAttribute("lista",servicio.buscarLibrosTodos());
+		return "listaLibros";
+	}
+	
+	@RequestMapping("/formularioInsertar")
+	public String formularioInsertar () {
+		return "formularioInsertar";
+	}
+	@RequestMapping("/insertar")
+	public String insertar(Libro libro, Model modelo, String nombreCategoria) {
+		
+		Categoria c=servicio.buscarCategoriaPorNombre(nombreCategoria);
+		libro.setCategoria(c);
+		servicio.insertarLibros(libro);
+		modelo.addAttribute("lista",servicio.buscarLibrosTodos());
+		return "listaLibros";
+	}
+	@RequestMapping("borrar")
+	public String Borrar(Model modelo, String isbn) {
+		Libro l=servicio.buscarLibrosPorISBN(isbn);
+		servicio.borrarLibros(l);
+		modelo.addAttribute("lista",servicio.buscarLibrosTodos());
+		return "listaLibros";
+	}
 }
---
+++ mvc2/src/main/java/es/avalon/servicios/ServicioLibros.java
@@ -0,0 +1,55 @@
+package es.avalon.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.avalon.dominio.Categoria;
+import es.avalon.dominio.Libro;
+import es.avalon.repositorios.CategoriaRepository;
+import es.avalon.repositorios.LibroRepository;
+@Service
+public class ServicioLibros {
+	
+		@Autowired
+		private LibroRepository repositorioLibro;
+		@Autowired
+		private CategoriaRepository repositorioCategoria;
+		
+		
+		public Categoria buscarCategoriaPorNombre(String nombre) {
+			return repositorioCategoria.buscarPorNombre(nombre);
+		}
+		public List<Libro> buscarLibrosTodos() {
+			return repositorioLibro.buscarTodos();
+		}
+		public Libro buscarLibrosPorISBN(String isbn) {
+			return repositorioLibro.buscarPorISBN(isbn);
+		}
+		public Libro buscarLibrosPorTitulo(String titulo) {
+			return repositorioLibro.buscarPorTitulo(titulo);
+		}
+		public List<Libro> OrdenarLibrosPorTitulo() {
+			return repositorioLibro.buscarTodosOrdenadosPorTitulo();
+		}
+
+		public List<Libro> OrdenarLibrosPorAutor() {
+			return repositorioLibro.buscarTodosOrdenadosPorAutor();
+		}
+		@Transactional
+		public void insertarLibros(Libro libro) {
+			repositorioLibro.insertar(libro);
+		}
+		@Transactional
+		public void salvarLibros(Libro libro) {
+			repositorioLibro.salvar(libro);
+		}
+		@Transactional
+		public void borrarLibros(Libro libro) {
+			repositorioLibro.borrar(libro);
+		}
+
+
+}
